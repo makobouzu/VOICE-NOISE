39942709d16cf331877d6f1606a5b124ccdad610
makobouzu <amako20@iamas.ac.jp>
2020-12-15 20:15:44 +0900

    update env

--- routes/db.js
+++ routes/db.js
@@ -12,7 +12,7 @@ const pool = new Pool({
     database: process.env.ENV_DB,
     user: process.env.ENV_USER,
     port: 5432,
-    password: process.env.ENV_PASSWORD,
+    password: process.env.ENV_PASSWORD
 });
 
 pool.connect();
