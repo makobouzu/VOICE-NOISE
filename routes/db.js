687c7ea704bf12d357088d1497e7780ce4aa90ee
makobouzu <amako20@iamas.ac.jp>
2020-12-15 20:23:20 +0900

    update env

--- routes/db.js
+++ routes/db.js
@@ -7,12 +7,12 @@ const connectionString = process.env.DATABASE_URL;
 // const connectionString = "postgres://amako:null@localhost:5432/voice_noise";
 
 const pool = new Pool({
-    // connectionString: connectionString
-    host: process.env.ENV_HOST,
-    database: process.env.ENV_DB,
-    user: process.env.ENV_USER,
-    port: 5432,
-    password: process.env.ENV_PASSWORD
+    connectionString: connectionString
+    // host: process.env.ENV_HOST,
+    // database: process.env.ENV_DB,
+    // user: process.env.ENV_USER,
+    // port: 5432,
+    // password: process.env.ENV_PASSWORD
 });
 
 pool.connect();
