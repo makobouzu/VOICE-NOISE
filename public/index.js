875c8a3d3ee5963d8349256d7a6bbb9ca20f1bda
makobouzu <mako.bouzu@gmail.com>
2021-02-02 20:58:08 +0900

    fix

--- public/index.js
+++ public/index.js
@@ -194,6 +194,27 @@ window.onload = function init(){
 //     __log('Stopped recording.');
 //     recNum = 1;
 // }
+document.getElementById("audioform").addEventListener('change', () => audioForm());
+
+function audioForm(){
+    const element = document.getElementById("audioForm")
+    var au = document.createElement('audio');
+
+    const file = element.files[0]
+    const fileReader = new FileReader()
+
+    fileReader.onload = () => {
+        const view = new DataView(fileReader.result)
+        const audioBlob = new Blob([view], { type: 'audio/wav' })
+        const myURL = window.URL || window.webkitURL
+        au.controls = true;
+        au.id = "audioComfirm"
+        au.src = myURL.createObjectURL(audioBlob)
+        audio_comfirm.appendChild(au);
+    }
+  
+    fileReader.readAsArrayBuffer(file);
+}
 
 function uploadRec() {
     gtag('event', 'submit_click', {
