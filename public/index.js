3bf6296f97f51dd59519bb750cae4016b020a4b1
makobouzu <mako.bouzu@gmail.com>
2021-02-04 16:17:36 +0900

    fix

--- public/index.js
+++ public/index.js
@@ -40,30 +40,15 @@ function audioConnect(){
         var input = audio_context.createMediaElementSource(audioSamples);
         rnnoise = new RNNoiseNode(audio_context);
         gainNode = audio_context.createGain();
-	    input.connect(gainNode);
-        audio_context.resume();
         __log('Media stream created.');
 
-        var CurY;
-var HEIGHT = window.innerHeight;
-
-// マウスが動いたら新しいY座標を取得し、
-// ゲインの値を設定する
-document.onmousemove = updatePage;
-
-function updatePage(e) {
-    CurY = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
-
-    gainNode.gain.value = CurY/HEIGHT;
-}
-
-
-        // rnnoise.connect(gainNode);
+        input.connect(rnnoise);
+        rnnoise.connect(gainNode);
         gainNode.connect(audio_context.destination);
         __log('Input connected to audio context destination.');
-        // updateNoise(rnnoise);
+        audio_context.resume();
+        updateNoise(rnnoise);
         __log("Voice: 0.5 - Noise: 0.5");
-        console.log(gainNode.gain.volume);
     }
     num = 1;
 }
