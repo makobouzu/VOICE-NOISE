b0baa4ef1887a55aca0aaf277aa3524ace6257b6
makobouzu <mako.bouzu@gmail.com>
2021-02-05 16:01:48 +0900

    fix

--- public/map.js
+++ public/map.js
@@ -62,7 +62,7 @@ map.on('load', () => {
                 .setPopup(new mapboxgl.Popup({ offset: 25 })
                 .setHTML(`<p class="fw-bold">${s.name}</p><audio id="marker_audio" src ="${s.path}" gtag('event', 'marker_click', {'event_category': 'marker_${s.name}', 'event_label': 'marker_${s.name}', 'non_interaction': true});" controls>`))
                 .addTo(map);
-                console.log(marker)
+                console.log(marker.id)
         });
     })
     .catch(err => {
