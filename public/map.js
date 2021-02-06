e4fb77f91767d9b18e7e20b43e4a099b51285c61
makobouzu <mako.bouzu@gmail.com>
2021-02-06 22:09:22 +0900

    fix

--- public/map.js
+++ public/map.js
@@ -93,8 +93,8 @@ map.on('load', () => {
                     });
                     buffer_marker = marker_num;
                 }else{
-                    alert("音源ファイルの読み込みに時間がかかっています。少々お待ちください。\nLoading the sound file now. Please wait a moment.");
                     popup.remove();
+                    alert("音源ファイルの読み込みに時間がかかっています。少々お待ちください。\nLoading the sound file now. Please wait a moment.");
                 }
             });
         });
