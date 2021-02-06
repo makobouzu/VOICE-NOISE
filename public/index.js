9ff2189a27e97efea727b50b27d52dd8ceafea0d
makobouzu <mako.bouzu@gmail.com>
2021-02-06 16:57:39 +0900

    fix

--- public/index.js
+++ public/index.js
@@ -71,7 +71,6 @@ function updateList(bufferList, sources, num){
     rnnoise.connect(gainNode);
     gainNode.connect(audio_context.destination);
     sources.splice(num, 1, source);
-    console.log(sources);
 }
 
 function uploadRec() {
