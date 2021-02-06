eb151b544ac559996b57e7abd952ecf9431355a4
makobouzu <mako.bouzu@gmail.com>
2021-02-06 15:42:13 +0900

    fix

--- public/map.js
+++ public/map.js
@@ -68,28 +68,31 @@ map.on('load', () => {
             num += 1;
             marker.getElement().addEventListener('click', () => {
                 const marker_num = marker.getElement().id.split('_')[1];
-                if(marker_play){
-                    sources[buffer_marker].stop(0);
-                    audio_context.suspend();
-                    updateList(bufferLoader.bufferList, sources, buffer_marker);
-                    document.getElementById("plus_marker").remove();
-                    sleep(100);
-                }
-                sources[marker_num].start();
-                audio_context.resume();
-                marker_play = true;
-                var plus = new mapboxgl.Marker({ "color": "#ff1622" })
-                    .setLngLat([s.location.x, s.location.y])
-                    .addTo(map);
-                plus._element.id = "plus_marker";
-                plus.getElement().addEventListener('click', () => {
-                    sources[marker_num].stop(0);
-                    audio_context.suspend();
-                    updateList(bufferLoader.bufferList, sources, marker_num);
-                    plus.remove();
-                    marker_play = false;
+                const promise = new Promise((resolve, reject) => {
+                    if(marker_play){
+                        sources[buffer_marker].stop(0);
+                        audio_context.suspend();
+                        updateList(bufferLoader.bufferList, sources, buffer_marker);
+                        document.getElementById("plus_marker").remove();
+                    }
                 });
-                buffer_marker = marker_num;
+                promise.then((result) => {
+                    sources[marker_num].start();
+                    audio_context.resume();
+                    marker_play = true;
+                    var plus = new mapboxgl.Marker({ "color": "#ff1622" })
+                        .setLngLat([s.location.x, s.location.y])
+                        .addTo(map);
+                    plus._element.id = "plus_marker";
+                    plus.getElement().addEventListener('click', () => {
+                        sources[marker_num].stop(0);
+                        audio_context.suspend();
+                        updateList(bufferLoader.bufferList, sources, marker_num);
+                        plus.remove();
+                        marker_play = false;
+                    });
+                    buffer_marker = marker_num;
+                });
             });
         });
     })
